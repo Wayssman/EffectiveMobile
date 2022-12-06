89cb13a70b86a10c7e04f94c8bd45d0b07c335eb
Alexandr <>
2022-12-06 03:01:46 +0300

    Initial Commit

---
+++ EffectiveMobile/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EffectiveMobile
+//
+//  Created by Alexandr on 06.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
